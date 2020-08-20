011c2aa34b3fa5a97d9acfa60d062b79912f5e06
Kabirul-Hasan-Sikder <kabir.webDev@gmail.com>
2020-08-20 22:04:00 +0600

    use of reduce

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import "./App.css";
 import Material from "./component/Material-UI-Compo/Material";
 // import Header from './component/Header/Header';
 import Navbar from "./component/Material-UI-Compo/Navbar";
+import Huku from "./component/Huku/Huku";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       {/* <Header/> */}
-      <div className="margin">
-        <Material />
-      </div>
+      <div className="margin">{/* <Material /> */}</div>
+      <Huku />
     </div>
   );
 }
---
+++ src/component/Huku/Huku.js
@@ -0,0 +1,29 @@
+import React, { useReducer } from "react";
+
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case "INCREASE":
+      return state + 1;
+      break;
+    case "DECREASE":
+      return state - 1;
+      break;
+    default:
+      return state;
+  }
+};
+
+function Huku() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div>
+      <h3>{state}</h3>
+      <button onClick={() => dispatch("INCREASE")}>Increase</button>
+      <button onClick={() => dispatch("DECREASE")}>Increase</button>{" "}
+    </div>
+  );
+}
+
+export default Huku;
