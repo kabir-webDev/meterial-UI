db0eebb7eee0783f52a8d0ae1b1d47c8a3fcf60d
Kabirul-Hasan-Sikder <kabir.webDev@gmail.com>
2020-08-25 23:40:26 +0600

    Conditional Function

--- src/component/Condition.js
+++ src/component/Condition.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { useState } from "react";
+import Walah from "./Walah";
+
+const If = ({ condition, children }) => {
+  if (condition) return children;
+  return null;
+};
 
 function Condition() {
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState(false);
   return (
     <>
       <div>
@@ -14,6 +20,9 @@ function Condition() {
       ) : (
         <div>In the langs In the false</div>
       )}
+      <If condition={show}>
+        <Walah />
+      </If>
     </>
   );
 }
---
+++ src/component/Walah.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Walah() {
+  return <div>This will be shown based on If component.</div>;
+}
+
+export default Walah;
