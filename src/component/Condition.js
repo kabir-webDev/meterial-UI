e485b158c39b9cc966d987929f4e564cf0f2be05
Kabirul-Hasan-Sikder <kabir.webDev@gmail.com>
2020-08-25 23:02:51 +0600

    Conditional Rendering

--- src/component/Condition.js
+++ src/component/Condition.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { useState } from "react";
 
 function Condition() {
-  return <div>Here we'll be doing Conditional Rendering</div>;
+  const [show, setShow] = useState(false);
+
+  const handleClick = () => {
+    console.log("Clicked bitch");
+  };
+
+  return (
+    <div>
+      (show ? <div>This is a rendering area.</div> :{" "}
+      <div>Here we'll be doing Conditional Rendering</div>)
+      <br />
+      {show}
+      <button onClick={() => handleClick(setShow(true))}>Click here</button>
+    </div>
+  );
 }
 
 export default Condition;
